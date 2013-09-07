5e3487ff91ce0cce4cfce8adb922dd14fe1a8e87
minionlove <lakshmibalram02@gmail.com>
2013-09-06 20:19:04 -0700

    Create class2.py

---
+++ class2.py
@@ -0,0 +1,38 @@
+import turtle
+turtle.Screen()
+lakshmi=turtle.Turtle()
+lakshmi.pencolor("hotpink")
+lakshmi.pensize(9)
+amudha=turtle.Turtle()
+amudha.pensize(8)
+amudha.pencolor("blue")
+amrita=turtle.Turtle()
+amrita=pensize(3)
+amrita.pencolor("green")
+
+WHAT I DID AT PYTHON
+ a_turtle.pencolor(pen_clr)
+>>> a_turtle.fd
+<bound method Turtle.forward of <turtle.Turtle object at 0x108cd0dd0>>
+>>> a_turtle.fd(90)
+>>> distance='100'
+>>> distance
+'100'
+>>> kint(distance)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'kint' is not defined
+>>> int(distance)
+100
+>>> a_turtle.left(90)
+>>> a_turtle.forward(90)
+>>> a_turtle.left(90)
+>>> a_turtle.forward(90)
+>>> a_turtle.left(90)
+>>> a_turtle.forward(90)
+>>> a_turtle=raw_input("make a square design")
+make a square design 90
+>>> a_turtl=raw_input(90)
+90
+>>> a_turtle=raw_input(90)
+90
